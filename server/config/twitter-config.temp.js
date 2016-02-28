d049d343c112ca5a67a44a9028e2dde70ce38832
Yifeng Wang <yeefom@gmail.com>
2016-02-28 12:40:50 -0800

    Add config file for twitter authentication

---
+++ server/config/twitter-config.temp.js
@@ -0,0 +1,10 @@
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+module.exports = client;
