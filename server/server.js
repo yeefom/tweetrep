7d32d4d78a3fa6c6020b1b7903ab0b9b407c243b
Yifeng Wang <yeefom@gmail.com>
2016-02-27 16:46:39 -0800

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var morgan = require('morgan');
+var body_parser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(body_parser.urlencoded({extended : true}));
+app.use(body_parser.json());
+app.use(express.static('client'));
+
+var server = app.listen(8080, function() {
+  console.log('Express server listening on port 8080');
+});
+
+module.exports = app;
