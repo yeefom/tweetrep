e134a018cf4f406e7c9ab877e38e8e7e56278bec
Yifeng Wang <yeefom@gmail.com>
2016-03-04 09:09:51 -0800

    Add more tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,12 +1,33 @@
 describe('Tweetrep server', function () {
   var request = require('supertest');
   var app = require('../server/server.js');
+  var util = require('../server/utilities/utilities.js');
 
   describe('API', function () {
     it('should send tweets for a GET request', function (done) {
       request(app)
       .get('/api/timeline/imdb')
       .expect(200, done);
+    });
+  });
+
+  describe('utilities', function () {
+    it('should get tweets from Twitter API', function () {
+      util
+      .getTweets("imdb")
+      .then(function (tweets) {
+        expect(tweets.length).to.eql(20);
+      });
+    });
+
+    it('should parse raw tweets', function () {
+      util
+      .getTweets("imdb")
+      .then(function (tweets) {
+        var parsed = util.parseTweets(tweets);
+        expect(parsed[0].profile.repScore).to.be.above(0);
+      });
+
     });
   });
 
